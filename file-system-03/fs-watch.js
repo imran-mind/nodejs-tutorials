949c858c45decac8ed4b65072af9df7ed63c3c65
imran-mind <shaikhimran115@gmail.com>
2023-09-18 19:05:41 +0530

    deleted file programs

--- file-system-03/fs-watch.js
+++
@@ -1,7 +0,0 @@
-const fs= require('fs');
-
-const currentFile = "./test-files";
-fs.watch(currentFile,(event,whatChange)=>{
-    console.log("Folder whatChange: ", whatChange);
-    console.log("Folder event: ", event);
-});
