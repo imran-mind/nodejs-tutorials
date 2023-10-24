9a4297b8e11c8453780e7ef8099efab3eee49af7
Shaikh Imran <shaikhimran115@gmail.com>
2023-10-25 00:05:55 +0530

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dbUrl = "mongodb+srv://shaikhimran115:CpaseQ42pbDkNv07@test-cluster.ugekaa1.mongodb.net/sample-db?retryWrites=true&w=majority"
+const dbUrl = "mongodb+srv://shaikhimran115:<password>@test-cluster.ugekaa1.mongodb.net/sample-db?retryWrites=true&w=majority"
 mongoose.
     connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=>{
